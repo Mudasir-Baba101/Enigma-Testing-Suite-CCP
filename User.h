cad7367ee38b92c543817fb027b0f3bf71099df2
Jyotkpatel <jyotkpatel12@gmail.com>
2026-03-27 15:53:33 +0530

    OOPs

--- User.h
+++ User.h
@@ -1,1 +1,17 @@
+#ifndef USER_H
+#define USER_H
 
+#include <string>
+
+class User {
+private:
+    std::string hashPassword(std::string password);
+    bool userExists(std::string username);
+
+public:
+    void registerUser();
+    bool loginUser(std::string& loggedInUser);
+    bool adminLogin();
+};
+
+#endif
